8551174281e2339724ed591491e8f9d062090e84
shebkhan <shebkhan@yahoo.ie>
2026-04-01 15:28:26 +0300

    Add backend-node/src/routes/index.ts

---
+++ backend-node/src/routes/index.ts
@@ -0,0 +1,8 @@
+export { default as authRoutes } from './auth.js';
+export { default as servicesRoutes } from './services.js';
+export { default as providersRoutes } from './providers.js';
+export { default as ordersRoutes } from './orders.js';
+export { default as paymentsRoutes } from './payments.js';
+export { default as reviewsRoutes } from './reviews.js';
+export { default as usersRoutes } from './users.js';
+export { default as tenantsRoutes } from './tenants.js';
